57faad7c05a32ea90be38929341ff2283dede3a4
David Davis <daviddavis@redhat.com>
2020-09-15 05:54:20 -0400

    Moving git checkout code to before_install.sh
    Moving the git checkout code to before_install.sh step so the install.sh
    script can be reused by other projects using pulplift in Travis.

---
+++ .travis/before_script.sh
@@ -0,0 +1,4 @@
+# For the source tests
+cd ..
+git clone https://github.com/pulp/pulpcore
+git clone https://github.com/pulp/pulp_file
--- .travis/install.sh
+++ .travis/install.sh
@@ -2,14 +2,14 @@
 
 set -xmveuo pipefail
 
-sudo apt update
+sudo apt update 0> /dev/null
 
 # Updating vagrant-sshfs is necessary to support CentOS 7,8 without repos pre-configured.
 curl --output vagrant-sshfs_1.3.4-1_all.deb http://ftp.br.debian.org/debian/pool/main/v/vagrant-sshfs/vagrant-sshfs_1.3.4-1_all.deb
-sudo apt install ./vagrant-sshfs_1.3.4-1_all.deb
+sudo apt install ./vagrant-sshfs_1.3.4-1_all.deb 0> /dev/null
 rm ./vagrant-sshfs_1.3.4-1_all.deb
 
-sudo apt install software-properties-common openssh-server vagrant-libvirt libvirt-daemon-system vagrant-sshfs qemu-utils qemu-kvm cpu-checker dnsmasq
+sudo apt install software-properties-common openssh-server vagrant-libvirt libvirt-daemon-system vagrant-sshfs qemu-utils qemu-kvm cpu-checker dnsmasq 0> /dev/null
 # This PPA doesn't exist for 20.04 (focal)
 if grep -i bionic /etc/os-release ; then
   sudo apt-add-repository --yes --update ppa:ansible/ansible
@@ -19,11 +19,11 @@ fi
 # https://github.com/ansible/ansible/pull/68211
 if grep -i focal /etc/os-release ; then
   curl --output ansible_2.9.9+dfsg-1_all.deb http://ftp.br.debian.org/debian/pool/main/a/ansible/ansible_2.9.9+dfsg-1_all.deb
-  sudo apt install ./ansible_2.9.9+dfsg-1_all.deb
+  sudo apt install ./ansible_2.9.9+dfsg-1_all.deb 0> /dev/null
   sudo rm ./ansible_2.9.9+dfsg-1_all.deb
 fi
 
-sudo apt install ansible
+sudo apt install ansible 0> /dev/null
 sudo kvm-ok
 sudo usermod -a -G libvirt $USER
 sudo systemctl enable --now ssh
@@ -37,10 +37,5 @@ free -m
 df -h
 df -hl
 cat /proc/cpuinfo
-sudo apt install virt-what
+sudo apt install virt-what 0> /dev/null
 sudo virt-what
-
-# For the source tests
-cd ..
-git clone https://github.com/pulp/pulpcore
-git clone https://github.com/pulp/pulp_file
